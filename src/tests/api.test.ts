7f5156a0b497ed6a1c12db9ffb1ccee001108c64
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-10-17 16:17:24 -0300

    prepaing the describes and installing @types/jest

--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -1,1 +1,33 @@
 import { homeMethod, aboutMethod, registerMethodRender, loginMethodRender } from "../controllers/getMethods";
+
+describe ("homeMethod GET test", (): void => {
+
+    it ("Should return the proper values for homeMethod", async (): Promise <void> => {
+
+    });
+
+});
+
+describe ("aboutMethod GET test", (): void => {
+
+    it ("Should return the proper values for aboutMethod", async (): Promise <void> => {
+
+    });
+
+});
+
+describe ("registerPage GET test", (): void => {
+
+    it ("Should return registerPage proper values", async (): Promise <void> => {
+
+    });
+
+});
+
+describe ("loginPage GET test", (): void => {
+
+    it ("Should return loginPage proper values", async (): Promise <void> => {
+
+    });
+
+});
